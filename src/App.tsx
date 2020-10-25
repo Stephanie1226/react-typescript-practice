f8d828c535f9a81e55fca104193fcae253237ef8
Chieh Wang <chiehwang1226@gmail.com>
2020-10-25 15:13:47 -0600

    use interface in component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+
 function App() {
+  const todos = [{id: 't1', text: 'Finist the course'}, {id: 't2', text: 'Cook dinner'}]
   return (
     <div className="App">
+      <TodoList items={todos} />
     </div>
   );
 }
